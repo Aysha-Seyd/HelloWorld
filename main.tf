19f31dcb71ccc4591779d5716d1103840f115ab2
Aysha Seyd <aysha.seyd@nationwide.co.uk>
2022-12-08 14:55:35 +0000

    initial commit

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
-terraform {
-  required_version = ">= 0.12.26"
-}
-output "hello_world" {
-  value = "Hello, World!"
+resource "local_file" "Hello_World" {
+  content = file("C:\\Users\\P502408\\OneDrive - Nationwide Building Society\\Desktop\\HelloWorld\\helloworld.py")
+  filename = "C:\\Users\\P502408\\OneDrive - Nationwide Building Society\\Desktop\\Hello_World\\app.py"
 }
