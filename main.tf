60d5e9927c84976387dcfd811b1c10ecb22467be
Aysha-Seyd <aysha.seyd@nationwide.co.uk>
2023-01-18 15:37:15 +0000

    initial commit

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
 resource "local_file" "Hello_World" {
-  content = file("C:\\Users\\P502408\\OneDrive - Nationwide Building Society\\Desktop\\HelloWorld\\helloworld.py")
-  filename = "C:\\Users\\P502408\\OneDrive - Nationwide Building Society\\Desktop\\Hello_World\\app.py"
+  content = file("/Users/Aysha.Seyd/Desktop/GitHub/HelloWorld/logginginfo.py")
+  filename = "app.py"
+}
+
+output "log_message" {
+  value = "./app.py"
 }
